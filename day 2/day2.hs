36518a74ca2a1889915ee64e4b7fead827d1c684
unknown <tiago.oliveira2@estudante.ufla.br>
2024-11-08 14:07:02 -0300

    day 2

--- day 2/day2.hs
+++ day 2/day2.hs
@@ -3,20 +3,26 @@ import System.IO (hFlush, stdout, readFile)
 {- ADVANCE OF CODE: 2017-}
 {-DAY 2/ PART 1-}
 
---Lê a string que contém as informações do arquivo e transforma o a entrada em uma lista de listas,
---onde os elementos de uma linha representam uma lista.
-readSpreadSheet :: String -> [[Int]]
-readSpreadSheet input = map (map read . words) $ lines input
+-- Lê a string que contém as informações do arquivo e transforma o a entrada em uma lista de listas,
+-- onde os elementos de uma linha representam uma lista.
+readSpreadsheet :: String -> [[Int]]
+readSpreadsheet input = map (map read . words) $ lines input
 
 solve :: [[Int]] -> Int
 solve xxs = sum [maximum xs - minimum xs | xs <- xxs]
 
+-- Percorre cada lista (que representa uma linha da planilha) e acha quais numeros são divisíveis e soma-se o resultadado
+solve2 :: [[Int]] -> Int
+solve2 spreadsheet = sum [ div x y | row <- spreadsheet, x <- row, y <- row, x /= y, mod x y == 0]
+ 
 main :: IO ()
 main = do
   
   input <- readFile "input.txt"  
-  let spreadSheet = readSpreadSheet input
+  let spreadsheet = readSpreadsheet input
   
-  print $ solve spreadSheet
+  print $ solve spreadsheet
+  print $ solve2 spreadsheet
   hFlush stdout
+  
 
